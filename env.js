18d459390b11f76265fa43be66983a73b7d9f8a2
mrredo <mrredogt@gmail.com>
2022-01-17 19:58:18 +0200

    j

--- env.js
+++ env.js
@@ -1,5 +1,7 @@
 module.exports = {
     "port": 8080,
-    //"mongo": "mongodb+srv://ez:vLhvfQHMw3TLR2f@cluster0.5d1st.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // small pc
-    "mongo": "mongodb+srv://ez:KsvGz2DWH.cufwt@cluster0.lek0b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" // big pc 
+    "mongo": "mongodb+srv://ez:vLhvfQHMw3TLR2f@cluster0.5d1st.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // small pc
+    //"mongo": "mongodb+srv://ez:KsvGz2DWH.cufwt@cluster0.lek0b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" // big pc 
 }
+//rihardsbembelis1com@gmail.com
+//awaeste1234
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = require('./env').port;
+const port = require('./env.js').port;
 const app = express();
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
@@ -62,7 +62,7 @@ app.get('/api/users/:type/:user', async function(req, res){
         "message": "Couldnt find the user",
         "code": "404"
       }
-    })
+    });
   }
 });
 
@@ -79,28 +79,60 @@ app.get('/api/users/:type/:user', async function(req, res){
     }
 */
 app.post('/api/post/channel', async function (req, res) {
+    let regexCHN = new RegExp("(https?:\/\/)?(www\.)?youtu((\.be)|(be\..{2,5}))\/((user)|(channel))\/");
     let user = req.body.user;
     let header = req.headers
-    if(header.key !== "e@#$%^&*(#$%^&*#$%^&ddde#$%^&*;Ds") return res.send("this is owner only post api!")
+    if(header.key !== "e@#$%^&*(#$%^&*#$%^&ddde#$%^&*;Ds") return res.send({
+      "error": {
+        "message": "OWNER_ONLY",
+        "code": "none"
+      }
+    });
     if(!user.channel_name ||
        !user.channel_url || 
        !user.channel_videos ||
-       !user.discord_id) return res.send("invalid parmeters! \n channel_name or channel_url or channel_videos or discord_id are required!")
+       !user.discord_id) return res.send({
+         "error": {
+           "message": "INVALID_PARAMETERS",
+           "code": "422"
+         }
+       });
     if(typeof user.channel_name !== 'string' || 
        typeof user.channel_url !== 'string'|
        typeof user.discord_id !== 'string' ||
-       typeof user.channel_videos !== 'object') return res.send('invalid types of things so idk i dont want it :)')
+       typeof user.channel_videos !== 'object') return res.send({
+        "error": {
+          "message": "INVALID_TYPE_OF_OBJECTS",
+          "code": "400"
+        }
+      });
+      if(regexCHN.test(user.channel_url) == false) return res.send({
+        "error": {
+          "message": "INVALID_CHANNEL_URL",
+          "code": "none"
+        }
+      })
     let search = await channelModel.findOne({
-      _id: user._id
+      _id: user.discord_id
     })
-    if(search) return res.send("user already exists on the api")
+    if(search) return res.send({
+      "error": {
+        "message": "CHANNEL_ALREADY_EXISTS",
+        "code": "409"
+      }
+    })
     new channelModel({
       "channel_url": user.channel_url,
       "_id": user.discord_id,
       "channel_name": user.channel_name,
       "channel_videos": []
     }).save()
-    return res.send('Channel has been added successfully');
+    return res.send({
+      "success": {
+        "message": "CREATED_CHANNEL",
+        "code": "201"
+      }
+    });
 });
 /*
  user: {
@@ -111,26 +143,61 @@ app.post('/api/post/channel', async function (req, res) {
  }
 */
 app.post('/api/post/video', async function (req, res) {
-  let user = req.body.user;
-  let header = req.headers
-  if(header.key !== "e@#$%^&*(#$%^&*#$%^&ddde#$%^&*;Ds") return res.send("this is owner only post api!")
-  if(!user.video_url ||
-     !user.video_title || 
-     !user.video_thumnail) return res.send("invalid parmeters! \n video_title or video_url or video_thumnail are required!")
-  if(typeof user.video_url !== 'string' || 
-     typeof user.video_title !== 'string'|
-     typeof user.video_thumnail !== 'string') return res.send('invalid types of things so idk i dont want it :)')
-  let search = await channelModel.findOne({
-    _id: user._id
+  let regexVID = new RegExp("^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?");
+  let video = req.body.video;
+  let header = req.headers;
+  if(header.key !== "e@#$%^&*(#$%^&*#$%^&ddde#$%^&*;Ds") return res.send({
+    "error": {
+      "message": "OWNER_ONLY",
+      "code": "none"
+    }
   })
-  if(search) return res.send("user already exists on the api")
-  new channelModel({
-    "channel_url": user.channel_url,
-    "_id": user.discord_id,
-    "channel_name": user.channel_name,
-    "channel_videos": []
-  }).save()
-  return res.send('Channel has been added successfully');
+  if(!video.video_url ||
+     !video.video_title || 
+     !video.video_thumbnail) return res.send({
+      "error": {
+        "message": "INVALID_PARAMETERS",
+        "code": "422"
+      }
+     })
+  if(typeof video.video_url !== 'string' || 
+     typeof video.video_title !== 'string'||
+     typeof video.video_thumbnail !== 'string') return res.send({
+      "error": {
+        "message": "INVALID_TYPE_OF_OBJECTS",
+        "code": "400"
+      }
+    })
+  if(regexVID.test(video.video_url) == false) return res.send({
+    "error": {
+      "message": "INVALID_VIDEO_URL",
+      "code": "none"
+    }
+    })
+  let search = await channelModel.findOne({
+    _id: video.discord_id
+  });
+  if(!search) return res.send({
+    "error": {
+      "message": "CHANNEL_NOT_FOUND",
+      "code": "404"
+    }
+  });
+  await channelModel.findOneAndUpdate({
+    _id: video.discord_id
+   }, {
+    $addToSet: { channel_videos: { 
+      video_url: video.video_url,
+      video_title: video.video_title || "invalid name",
+      video_thumbnail: video.video_thumbnail
+    }}
+  });
+  return res.send({
+    "success": {
+      "message": "ADDED_VIDEO",
+      "code": "201"
+    }
+  });
 });
 
 app.set('view engine', 'ejs');
@@ -140,6 +207,9 @@ app.get('/', function(req, res) {
 });
 app.get('/qna', function(req, res) {
   res.render('qna.ejs');
+});
+app.get('/rules', function(req, res) {
+  res.render('rules.ejs');
 });
 
 

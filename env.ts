002cc5c8440433c80ada81b9a925adc9c6062638
mrredo <74524695+mrredo@users.noreply.github.com>
2022-01-22 21:46:05 +0200

    Delete env.ts

--- env.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "port": 8080,
-    "mongo": "mongodb+srv://ez:vLhvfQHMw3TLR2f@cluster0.5d1st.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    "key": "6@8Q-p%dE*rBYx!C^&(%^*JNndde@"
-}
-//rihardsbembelis1com@gmail.com
-//awaeste1234
